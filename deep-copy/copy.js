408cff1bd508a18a0b5542561aa1af5d861270d7
realgarny <aseev.nik1@yandex.ru>
2024-03-27 18:31:02 +0500

    fixed deep-copy

--- deep-copy/copy.js
+++ deep-copy/copy.js
@@ -1,29 +1,29 @@
+function deepCopy(obj, seen = new WeakMap()) {
+    if (seen.has(obj)) return seen.get(obj);
 
-const deepCopy = (obj, seen=[]) => {
-    if (typeof(obj) !== "object") return obj;
-    if (seen.length === 0) seen.push(obj);
+    let copy;
 
-    let value;
-    let temp = Array.isArray(obj) ? [] : {};
-    
-    for(const key in obj) {
-        value = obj[key];
+    if (obj instanceof Date) {
+        copy = new Date(obj);
+    } else if (obj instanceof Map) {
+        copy = new Map(Array.from(obj.entries()));
+    } else if (obj instanceof Set) {
+        copy = new Set(Array.from(obj.values()));
+    } else if (typeof obj === 'object' && obj !== null) {
+        copy = Array.isArray(obj) ? [] : {};
 
-        if(obj[key].constructor.name !== "Object") {
-            temp[key] = value;
-        } else {
-            for(let i = 0; i < seen.length; i++)
-            {
-                if(seen[i] === value)
-                {
-                    return seen[i];
-                }
+        seen.set(obj, copy);
+
+        for (let prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                copy[prop] = deepCopy(obj[prop], seen);
             }
-            temp[key] = deepCopy(value);
-            seen.push(temp[key]);
         }
+    } else {
+        copy = obj;
     }
-    return temp;
+
+    return copy;
 }
 
 const testObject = {
